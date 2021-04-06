01fdbaf1699313236e416c7e523b73014529d3ef
jean553 <lelievrejean82@gmail.com>
2021-04-06 23:22:36 +0200

    reorganize code and separate code into modules

--- treasure-island-server/src/main.rs
+++ treasure-island-server/src/main.rs
@@ -3,12 +3,17 @@
 extern crate bincode;
 extern crate serde_derive;
 
-use serde_derive::{Serialize};
+mod message;
+mod threads;
 
-use std::net::{
-    TcpListener,
-    TcpStream,
+use message::Message;
+
+use threads::{
+    forward_messages_from_global_receiver_to_all_clients,
+    send_message_into_client_stream,
 };
+
+use std::net::TcpListener;
 
 use std::sync::mpsc::{
     Sender,
@@ -22,78 +27,6 @@ use std::sync::{
     Mutex,
     Arc,
 };
-
-use std::io::Write;
-
-/// Structure of a message between the server and the client.
-/// NOTE: only one field wrapped into a structure for now, but more fields will be added later
-///
-/// FIXME: for tests only, we assume the map is 32 tiles-long, as it is not possible to serialize
-/// arrays of more than 32 values; we should divide the map into separated arrays to sent its data;
-/// or we should provide our own serialization function for that longer array
-#[derive(Serialize, Clone, Copy)]
-struct Message {
-    map: [u8; 32]
-}
-
-/// Contains the whole code of a dedicated thread. Continuously forwards the messages from the
-/// global receiver to all the clients out senders (so to all the clients individually).
-///
-/// # Args:
-///
-/// `global_receiver` - the unique global receiver that centralizes all messages for all clients
-/// `clients_out_senders_mutex_arc` - protected pointer to the array of clients out senders
-fn forward_messages_from_global_receiver_to_all_clients(
-    global_receiver: Receiver<Message>,
-    clients_out_senders_mutex_arc: Arc<Mutex<Vec<Sender<Message>>>>,
-) {
-
-    loop {
-
-        /* blocking until messages come from the global receiver */
-        let message = global_receiver.recv();
-
-        /* FIXME: #5 investigate why the recv() function returns an error
-           the first time it is executed, causing the whole thread to panic and to stop;
-           simply ignore the error for now */
-        if message.is_err() {
-            continue;
-        }
-
-        let message = message.unwrap();
-
-        /* blocking until there is no use of the clients out senders array from another thread */
-        let clients_out_senders_mutex_guard = clients_out_senders_mutex_arc.lock().unwrap();
-
-        let clients_out_senders = &*clients_out_senders_mutex_guard;
-
-        for client_out_sender in clients_out_senders {
-            client_out_sender.send(message).unwrap();
-        }
-    }
-}
-
-/// Contains the whole code of a dedicated thread. This thread is spawn once per new client.
-/// Continuously forwards the client dedicated receiver to the client dedicated stream.
-///
-/// # Args:
-///
-/// `client_receiver` - the dedicated receiver of the client, to get messages to send
-/// `client_stream` - the dedicated stream of the client, to send message to him
-fn send_message_into_client_stream(
-    client_receiver: Receiver<Message>,
-    mut client_stream: TcpStream,
-) {
-
-    loop {
-
-        /* blocks until message comes from the current client sender */
-        let message = client_receiver.recv().unwrap();
-
-        let data: Vec<u8> = bincode::serialize(&message).unwrap();
-        client_stream.write(&data).unwrap();
-    }
-}
 
 fn main() {
 
@@ -179,17 +112,7 @@ fn main() {
 
         for client_out_sender in client_out_senders {
 
-            /* FIXME: does not send 0 values as the client uses the first index
-               value of the sent data to know if it should consider the message or not */
-            const DEFAULT_VALUE: u8 = 10;
-            const MAP_LENGTH: usize = 32;
-            let message = Message {
-                map: [
-                    DEFAULT_VALUE;
-                    MAP_LENGTH
-                ]
-            };
-
+            let message = Message::new();
             client_out_sender.send(message).unwrap();
         }
     }
